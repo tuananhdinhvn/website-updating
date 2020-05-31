d4d4919c375d9bb3d7912c8306309fe0db11fd21
= <=>
2020-05-31 11:55:33 +0700

    website updating announcement

---
+++ Website_update_announcement.php
@@ -0,0 +1,162 @@
+
+<!doctype html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="vi"> <![endif]-->
+<!--[if IE 9 ]><html class="ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" lang="vi"> <!--<![endif]-->
+<head>
+	<!-- Basic page needs ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta property="og:locale" content="vi_VN">
+    <meta name="revisit" content="1 days">
+    <meta name="robots" content="index,follow">
+
+    <link rel="shortcut icon" href="https://tuananhdinh.vn/public/template/iset/favicon.png" type="image/png" />
+	<meta property="og:image" content="https://www.tuananhdinh.vn/public/template/iset/favicon.png" />
+	<meta property="og:image:secure_url" content="https://www.tuananhdinh.vn/public/template/iset/favicon.png" />
+
+    <!-- Title and description ================================================== -->
+    <title>
+		ISET education
+    </title>
+
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <!-- CSS ================================================== -->
+    <link href='//theme.hstatic.net/1000244835/1000376583/14/timber.scss.css?v=471' rel='stylesheet' type='text/css' media='all' />
+    <link href='//theme.hstatic.net/1000244835/1000376583/14/suplo-style.scss.css?v=471' rel='stylesheet' type='text/css' media='all' />
+    <link href='//theme.hstatic.net/1000244835/1000376583/14/owl.transitions.css?v=471' rel='stylesheet' type='text/css' media='all' />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&amp;subset=vietnamese" rel="stylesheet">
+
+  
+    <style>
+        body,
+        html {
+        height: 100%;
+        }
+
+		.text-under{
+			font-size: 10px !important;
+			font-weight: bold
+		}
+    </style>
+	
+	
+</head>
+
+<body id="he-thong-cua-hang-trang-phuc-va-dung-cu-the-thao-suplo" class=" template-index" >
+
+
+
+
+	<div id="PageContainer" class="is-moved-by-drawer">
+		<main class="main-content" role="main">           
+    
+            <section id="home-featured-products">
+				<div class="wrapper">
+					<div class="inner" style="text-align: center">
+						<img width="200px" src="https://rizing.com/wp-content/uploads/2019/10/201909-EAM-CI-Website-Icons-Business-Process-Re-Engnrg-800x800.png" alt="">
+						<h2 class="section-title text-center">
+							We 're updating the site<br/>
+							Please check back soon 
+						</h2>
+			
+						<div class="home-collection-countdown text-center">
+							<div class="countdown-days">
+								<div id="days">
+									0
+								</div>
+								<div class="text-under">
+									Days
+								</div>
+							</div>
+							<div class="countdown-hrs">
+								<div id="hrs">
+									0
+								</div>
+								<div class="text-under">
+									Hours
+								</div>
+							</div>
+							<div class="countdown-mins">
+								<div id="mins">
+									0
+								</div>
+								<div class="text-under">
+									Minutes
+								</div>
+							</div>
+							<div class="countdown-secs">
+								<div id="secs">
+									0
+								</div>
+								<div class="text-under">
+									Seconds
+								</div>
+							</div>
+						</div>
+			
+					</div>
+				</div>
+			</section>
+
+        </main>
+    </div>
+
+
+
+
+
+
+
+
+
+   
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("June 8, 2020 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+	// Get todays date and time
+	var now = new Date().getTime();
+
+	// Find the distance between now an the count down date
+	var distance = countDownDate - now;
+
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Display the result in the element with id="demo"
+	document.getElementById("days").innerHTML = days;
+
+	document.getElementById("hrs").innerHTML = hours;
+
+	document.getElementById("mins").innerHTML = minutes;
+
+	document.getElementById("secs").innerHTML = seconds;
+
+	// If the count down is finished, write some text
+	//if (distance < 0) {
+	//      clearInterval(x);
+	//    document.getElementById("flash-sale-status").innerHTML = "Flash Sale đã kết thúc";
+	//}
+}, 1000);
+</script>
+
+
+
+
+
+
+
+</body>
+</html>
